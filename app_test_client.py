701c55210983781311d11855d0eb24facf6e2ad6
Justin Plock <jplock@gmail.com>
2010-03-27 10:57:23 -0400

    Whitespace cleanup

--- app_test_client.py
+++ app_test_client.py
@@ -7,18 +7,20 @@ import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
-import models
 import authapp
+import models
 
 # test@flomosa.com
 KEY = '4ef3e685-37c1-43f9-ae03-0a21523051c6'
 SECRET = '1913b245-18ae-4caa-a491-cedd2e471a50'
 
+
 class TestHandler(authapp.SecureRequestHandler):
     def get(self):
         logging.debug('Begin TestHandler.get() method')
 
-        client = models.Client(key_name=KEY, email_address='test@flomosa.com',
+        client = models.Client(key_name=KEY,
+            email_address='test@flomosa.com',
             password=self.encrypt_string('test'))
         client.first_name = 'Test'
         client.last_name = 'Test'
