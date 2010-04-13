126edc36a8de91e1fc90b824386c31696c283b89
Justin Plock <jplock@gmail.com>
2010-04-12 21:27:50 -0400

    Added comments to settings file

--- settings.py
+++ settings.py
@@ -2,11 +2,26 @@
 # Copyright 2010 Flomosa, LLC
 #
 
-COOKIE_NAME = 'flomosa'
+# Application name on AppEngine
+APPLICATION_NAME = 'flomosa'
+
+# Name of the secure cookie
+COOKIE_NAME = APPLICATION_NAME
+
+# Salt to use for encrypting the secure cookie
 COOKIE_SECRET = 'F10m0sA'
 
+# Email address to receive feedback at
 FEEDBACK_EMAIL = 'feedback@flomosa.com'
-EMAIL_DOMAIN = 'flomosa.appspotmail.com'
+
+# Full email domain name on AppEngine
+EMAIL_DOMAIN = '%s.appspotmail.com' % APPLICATION_NAME
+
+# Email address on AppEngine to receive feedback at
 FEEDBACK_FORWARDER_EMAIL = 'feedback@%s' % EMAIL_DOMAIN
+
+# Maximum number of reminders to send
 REMINDER_LIMIT = 10
-REMINDER_DELAY = 43200 # send reminders every 12 hours
+
+# How often to send a reminder email (in seconds)
+REMINDER_DELAY = 43200 # 12 hours
