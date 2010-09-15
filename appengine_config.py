caa678ae4391c353e5d44aaa457e2c10728d4b81
jplock <jplock@gmail.com>
2010-09-15 17:07:16 -0400

    Updated copyright headers

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,9 @@
+# -*- coding: utf8 -*-
+#
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 #
 # Copyright 2010 Flomosa, LLC
+# All Rights Reserved.
 #
 
 def webapp_add_wsgi_middleware(app):
--- flomosa/queue/execution_process.py
+++ flomosa/queue/execution_process.py
@@ -1,5 +1,9 @@
+# -*- coding: utf8 -*-
+#
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 #
 # Copyright 2010 Flomosa, LLC
+# All Rights Reserved.
 #
 
 from datetime import datetime
